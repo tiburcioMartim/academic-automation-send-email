90f93d783846ffc8c2ef91961728cefa625f5603
Martim Tiburcio <tiburciomartim@gmail.com>
2024-11-26 15:41:23 -0300

    adjusts in code

--- main.py
+++ main.py
@@ -1,39 +1,33 @@
-# Necessário para ler arquivos em Excel: # pip install openpyxl
-import pandas as pd             # pip install pandas
-import win32com.client as win32    # pip install pywin32 | # Integra o python com o windows:
+                                    # pip install openpyxl    | Read files in Excel
+import pandas as pd                 # pip install pandas      | To work with tables
+import win32com.client as win32     # pip install pywin32     | Integrating Python with windows
 
-
-# importar base de dados
+# To import data base
 tabela_vendas = pd.read_excel("Vendas.xlsx")
 
-
-# visualizar base de dados
+# To view data base
 pd.set_option('display.max_columns', None)
 print(tabela_vendas)
 print('-' * 50)
 
-
-# faturamento por loja
-faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
-print(faturamento)
+# Billing per store
+earnings = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+print(earnings)
 print('-' * 50)
 
-
-# quantidade de produtos vendidos por loja
-quantidades = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
-print(quantidades)
+# Quantity of products sell per store
+quantity = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+print(quantity)
 print('-' * 50)
 
-
-# ticket medio por produto em cada loja
-ticket_medio = (faturamento['Valor Final'] / quantidades['Quantidade']).to_frame()
+# Averige ticket per products in each store
+ticket_medio = (earnings['Valor Final'] / quantity['Quantidade']).to_frame()
 ticket_medio = ticket_medio.rename(columns={0: 'Tiket Médio'})
 print(f'\n\n{ticket_medio}')
 print('-' * 50)
 
-
 try:
-    # enviar email com o relatório
+    # Send email with report
     outlook = win32.Dispatch('Outlook.Application')
     mail = outlook.CreateItem(0)
     mail.To = 'tiburciomartim@gmail.com'
@@ -43,10 +37,10 @@ try:
         
         <p>Segue o relatório de vendas por cada loja.</p>
         <p>Faturamento:</p>
-        {faturamento.to_html(formatters={'Valor Final': 'R$ {:,.2f}'.format})}
+        {earnings.to_html(formatters={'Valor Final': 'R$ {:,.2f}'.format})}
         
         <p>Quantidade Vendida:</p>
-        {quantidades.to_html()}
+        {quantity.to_html()}
         
         <p>Ticket Médio dos Produtos em cada Loja:</p>
         {ticket_medio.to_html(formatters={'Tiket Médio': 'R$ {:,.2f}'.format})}
@@ -56,9 +50,9 @@ try:
         <p>Att.,</p>
         <p>Tiburcio</p>
     '''
-
     mail.Send()
     print('\nEmail enviado.')
+
 except Exception as e:
     print(f"Falha na conexão. {e}")
 
